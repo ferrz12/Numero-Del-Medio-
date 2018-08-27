74446b437ace56dcd0e995cea79abad6591bd5c6
fernando <fernando@fernando.com>
2018-08-27 11:30:10 -0300

    ingreso de datos

---
+++ numeroDelMedio/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1;
+    int num2;
+    int num3;
+
+    printf("Ingrese numero 1: \n");
+    scanf("%d", &num1);
+
+    printf("Ingrese numero 2: \n");
+    scanf("%d", &num2);
+
+    printf("Ingrese numero 3: \n");
+    scanf("%d", &num3);
+
+    return 0;
+}
