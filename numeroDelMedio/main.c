5f61a9e3d0da9e965dca08654401564eb9cf0be9
fernando <fernando@fernando.com>
2018-08-27 11:44:52 -0300

    carga de maximos y minimos

--- numeroDelMedio/main.c
+++ numeroDelMedio/main.c
@@ -6,15 +6,39 @@ int main()
     int num1;
     int num2;
     int num3;
+    int maximo;
+    int minimo;
+    int numeroDelMedio;
 
     printf("Ingrese numero 1: \n");
     scanf("%d", &num1);
 
+    maximo = num1;
+    minimo = num1;
+
     printf("Ingrese numero 2: \n");
     scanf("%d", &num2);
 
+    if(num2 < minimo){
+        minimo = num2;
+    }else{
+        if(num2 > maximo){
+            maximo = num2;
+        }
+    }
+
     printf("Ingrese numero 3: \n");
     scanf("%d", &num3);
+
+    if(num3 < minimo){
+        minimo = num3;
+    }else{
+        if(num3 > maximo){
+            maximo = num3;
+        }
+    }
+
+
 
     return 0;
 }
